4aa175cd648a5508209da1ab8623b2f579cb2709
ozon671game <61536382+ozon671game@users.noreply.github.com>
2020-06-04 09:42:09 +0300

    Delete Game.java

--- Game.java
+++
@@ -1,9 +0,0 @@
-public interface Game
-{
-    void initGame();
-    void move();// движение квадратов
-    void checkCollisionsWall();// проверка на столкновения квадрата со  стеной
-    void  changeColor (int i);// смена цвета
-    void checkCollisions();// проверка на столкновения
-
-}
